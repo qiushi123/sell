ef64595e2fc3061af7b0a2e85b7366f9f64c6ccb
qiushi123 <2501902696@qq.com>
2018-04-28 13:06:31 +0800

    pv2048表格自增

--- src/main/java/com/qcl/dataobject/Article.java
+++ src/main/java/com/qcl/dataobject/Article.java
@@ -4,6 +4,7 @@ import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
@@ -17,7 +18,7 @@ import lombok.Data;
 @Data//避免重复写get和set，tostring
 public class Article {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long aid;
 
     private String title;
