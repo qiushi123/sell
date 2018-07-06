a52e0c24af7de90bbd11c12bf31eac3f3f77015f
qiushi123 <2501902696@qq.com>
2018-07-06 09:29:14 +0800

    手机匹配正则表达式过期问题解决

--- src/main/java/com/qcl/xiyiji/form/AuthCodeForm.java
+++ src/main/java/com/qcl/xiyiji/form/AuthCodeForm.java
@@ -20,9 +20,14 @@ public class AuthCodeForm {
     private String openid;
     @NotEmpty(message = "姓名不能为空")
     private String name;
+
+
+    //@Pattern(regexp = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$", message = "手机号码格式错误")
+    //上面面这个正则表达式过期了，用下面这个最新的
+    @Pattern(regexp = "^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\\d{8}$", message = "手机号码格式错误")
     @NotEmpty(message = "手机号不能为空")
     @Size(min = 11, max = 11, message = "手机号必须是11位")
-    @Pattern(regexp = "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\\d{8}$", message = "手机号码格式错误")
+
     private String phone;
 
 }
