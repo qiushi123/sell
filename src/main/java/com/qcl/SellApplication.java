e13c538bf926dddb9f5484a89942cc8acf896c8c
qiushi123 <2501902696@qq.com>
2018-07-31 10:50:46 +0800

    上传图片功能bug修改3

--- src/main/java/com/qcl/SellApplication.java
+++ src/main/java/com/qcl/SellApplication.java
@@ -2,7 +2,11 @@ package com.qcl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.servlet.MultipartConfigElement;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +14,15 @@ public class SellApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellApplication.class, args);
+    }
+    /**
+     * 文件上传临时路径
+     */
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation("/usr");
+        return factory.createMultipartConfig();
     }
 
 }
